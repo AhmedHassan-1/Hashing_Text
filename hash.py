450ef010cd5c82310860d8f5dde562fb67995737
Ahmed Hassan <ahmed4321medohasan@gmail.com>
2023-11-27 18:46:46 +0200

    Upload Script

---
+++ hash.py
@@ -0,0 +1,43 @@
+import os
+
+try:
+    import pyperclip
+    import hashlib
+    import argparse
+except:
+    os.system("pip  install argparse")
+    os.system("pip  install hashlib")
+    os.system("pip  install pyperclip")
+    os.system("clear")
+
+parser = argparse.ArgumentParser(description="Hashing Text")
+parser.add_argument(
+    "-t", "--text", type=str, help="Text that will be hashed", required=True
+)
+
+parser.add_argument(
+    "-m",
+    "--Method",
+    type=str,
+    help="defult: Md5",
+    choices=[
+        "md5",
+        "sha224",
+        "sha256",
+        "sha384",
+        "sha512",
+        "sha3_224",
+        "sha3_256",
+        "sha3_384",
+        "sha3_512",
+        "snake_128",
+        "snake_256",
+    ],
+    default="md5",
+    required=False,
+)
+args = parser.parse_args()
+hash_function = getattr(hashlib, args.Method)
+hashed = hash_function(str(args.text).encode()).hexdigest()
+pyperclip.copy(hashed)
+print(f"{args.Method} Hash is :", hashed)
